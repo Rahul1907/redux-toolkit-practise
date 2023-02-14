db9ca2b0df9eb269bd7d85f803893baae58404e4
Rahul1907 <rahul.19797@gmail.com>
2023-02-15 00:24:06 +0530

    redux shopping cart done

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,39 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { add } from '../store/cartSlice'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { add } from '../store/cartSlice';
+import { fetchProducts } from '../store/productSlice';
+import { STATUSES } from '../store/productSlice';
 
 const Products = () => {
-    const [products, setProducts] = useState([])
     const dispatch = useDispatch();
+    const { data: products, status } = useSelector((state) => state.product);
+
     useEffect(() => {
-        fetchProducts()
-    }, [])
+        dispatch(fetchProducts());
+        
+    }, []);
 
-    const fetchProducts = async () => {
-        let products = await fetch('https://fakestoreapi.com/products')
-            .then(res => res.json())
-            .then(json => json)
+    const handleAdd = (product) => {
+        dispatch(add(product));
+    };
 
-        setProducts(products)
+    if (status === STATUSES.LOADING) {
+        return <h2>Loading....</h2>;
     }
 
-    const onAddToCart = (product) => {
-        dispatch(add(product))
+    if (status === STATUSES.ERROR) {
+        return <h2>Something went wrong!</h2>;
     }
-
     return (
-        <div className='productsWrapper'>
-            {products.map((product) => {
-
-                return (<div className='card' key={product.id}>
-                    <img src={product.image} />
+        <div className="productsWrapper">
+            {products.map((product) => (
+                <div className="card" key={product.id}>
+                    <img src={product.image} alt="" />
                     <h4>{product.title}</h4>
                     <h5>{product.price}</h5>
-                    <button className='btn' onClick={() => { onAddToCart(product) }}> Add to cart </button>
-                </div>)
-            })}
+                    <button onClick={() => handleAdd(product)} className="btn">
+                        Add to cart
+                    </button>
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default Products
+export default Products;
